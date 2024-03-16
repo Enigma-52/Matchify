38351a327a84802985d9438ddab4a7e8ddb2429c
Rohit Singh <rohit.singh.33521@gmail.com>
2024-03-16 17:04:13 +0530

    fix

--- public/scripts/pricing.js
+++ public/scripts/pricing.js
@@ -1,4 +1,21 @@
-function showPaymentDetails(button) {
-    var paymentDetails = button.nextElementSibling;
-    paymentDetails.style.display = 'block';
+function toggleCardClick(button) {
+    // Get the parent pricing card element
+    var card = button.closest('.pricing-card');
+    // Toggle the 'clicked' class on the pricing card
+    card.classList.toggle('clicked');
+
+    // Toggle the 'permanent' class on features and payment details
+    var features = card.querySelector('.features');
+    features.classList.toggle('permanent');
+
+    // Access the list items within the features section
+    var featureItems = features.querySelectorAll('li');
+    featureItems.forEach(function(item) {
+        item.classList.toggle('permanent');
+    });
+
+    var paymentDetails = card.querySelector('.payment-details');
+    paymentDetails.classList.toggle('permanent');
+
+    button.style.display = 'none';
 }
